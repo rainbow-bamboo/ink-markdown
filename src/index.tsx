3434f19935a4374d20c30d0255e890600017b4a8
Jeff Escalante <jescalan@users.noreply.github.com>
2024-01-10 16:07:51 -0500

    trim output to avoid extra newline, use marked-terminal fork with better highlighting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { marked } from 'marked';
 import { Text } from 'ink';
+// @ts-ignore - temporary fix for using a fork
 import { markedTerminal, TerminalRendererOptions } from 'marked-terminal';
 
 export type Props = TerminalRendererOptions & {
@@ -9,5 +10,5 @@ export type Props = TerminalRendererOptions & {
 
 export default function Markdown({ children, ...options }: Props) {
   marked.use(markedTerminal(options));
-  return <Text>{marked.parse(children) as string}</Text>;
+  return <Text>{(marked.parse(children) as string).trim()}</Text>;
 }
